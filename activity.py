b2a9b06250c5fc81577312182e770d90489039b0
Sangeerthan21 <kandeepann5@gmail.com>
2024-12-18 18:00:18 +0530

    Alphabet check

---
+++ activity.py
@@ -0,0 +1,108 @@
+txt = input("Enter any number or letters : ")
+
+if "a" in txt:
+     print("Your input has alphabetic letters.")
+elif "b" in txt:
+     print("Your input has alphabetic letters.")
+elif "c" in txt:
+     print("Your input has alphabetic letters.")
+elif "d" in txt:
+     print("Your input has alphabetic letters.")
+elif "e" in txt:
+     print("Your input has alphabetic letters.")
+elif "f" in txt:
+     print("Your input has alphabetic letters.")
+elif "g" in txt:
+     print("Your input has alphabetic letters.")
+elif "h" in txt:
+     print("Your input has alphabetic letters.")
+elif "i" in txt:
+     print("Your input has alphabetic letters.")
+elif "j" in txt:
+     print("Your input has alphabetic letters.")
+elif "k" in txt:
+     print("Your input has alphabetic letters.")
+elif "l" in txt:
+     print("Your input has alphabetic letters.")
+elif "m" in txt:
+     print("Your input has alphabetic letters.")
+elif "n" in txt:
+     print("Your input has alphabetic letters.")
+elif "o" in txt:
+     print("Your input has alphabetic letters.")
+elif "p" in txt:
+     print("Your input has alphabetic letters.")
+elif "q" in txt:
+     print("Your input has alphabetic letters.")
+elif "r" in txt:
+     print("Your input has alphabetic letters.")
+elif "s" in txt:
+     print("Your input has alphabetic letters.")
+elif "t" in txt:
+     print("Your input has alphabetic letters.")
+elif "u" in txt:
+     print("Your input has alphabetic letters.")
+elif "v" in txt:
+     print("Your input has alphabetic letters.")
+elif "w" in txt:
+     print("Your input has alphabetic letters.")
+elif "x" in txt:
+     print("Your input has alphabetic letters.")
+elif "y" in txt:
+     print("Your input has alphabetic letters.")
+elif "z" in txt:
+     print("Your input has alphabetic letters.")
+elif "A" in txt:
+     print("Your input has alphabetic letters.")
+elif "B" in txt:
+     print("Your input has alphabetic letters.")
+elif "C" in txt:
+     print("Your input has alphabetic letters.")
+elif "D" in txt:
+     print("Your input has alphabetic letters.")
+elif "E" in txt:
+     print("Your input has alphabetic letters.")
+elif "F" in txt:
+     print("Your input has alphabetic letters.")
+elif "G" in txt:
+     print("Your input has alphabetic letters.")
+elif "H" in txt:
+     print("Your input has alphabetic letters.")
+elif "I" in txt:
+     print("Your input has alphabetic letters.")
+elif "J" in txt:
+     print("Your input has alphabetic letters.")
+elif "K" in txt:
+     print("Your input has alphabetic letters.")
+elif "L" in txt:
+     print("Your input has alphabetic letters.")
+elif "M" in txt:
+     print("Your input has alphabetic letters.")
+elif "N" in txt:
+     print("Your input has alphabetic letters.")
+elif "O" in txt:
+     print("Your input has alphabetic letters.")
+elif "P" in txt:
+     print("Your input has alphabetic letters.")
+elif "Q" in txt:
+     print("Your input has alphabetic letters.")
+elif "R" in txt:
+     print("Your input has alphabetic letters.")
+elif "S" in txt:
+     print("Your input has alphabetic letters.")
+elif "T" in txt:
+     print("Your input has alphabetic letters.")
+elif "U" in txt:
+     print("Your input has alphabetic letters.")
+elif "V" in txt:
+     print("Your input has alphabetic letters.")
+elif "W" in txt:
+     print("Your input has alphabetic letters.")
+elif "X" in txt:
+     print("Your input has alphabetic letters.")
+elif "Y" in txt:
+     print("Your input has alphabetic letters.")
+elif "Z" in txt:
+     print("Your input has alphabetic letters.")
+else:
+     print("Your input has no alphabetic letters")
